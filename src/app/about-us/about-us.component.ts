c864e6bda4616a7f2684a5363ea2550641cc2140
Akhil9514 <55031786+Akhil9514@users.noreply.github.com>
2025-02-07 17:06:24 +0530

    about-us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ImageSliderComponent } from '../image-slider/image-slider.component';
+import { ToastComponent } from '../toast/toast.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [ImageSliderComponent],
+  imports: [ImageSliderComponent, ToastComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
