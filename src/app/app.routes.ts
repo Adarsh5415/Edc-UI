553a0365cdc7bb81dccde8fc5aa0de59f1a010ca
Akhil9514 <55031786+Akhil9514@users.noreply.github.com>
2025-02-07 12:00:25 +0530

    csf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { ImageSliderComponent } from './image-slider/image-slider.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
     {path:'',component:UserDashboardComponent},
     {path:'images',component:ImageSliderComponent},
     {path:'about-us',component:AboutUsComponent},
+    {path:'gallery',component:GalleryComponent},
 ];
